1194379124ce401f5c56fdec05172c9e81a18daa
Noah Purdy <noahpurdy.dev@gmail.com>
2026-07-12 12:56:18 +0900

    Add logic for restarting flashcards.

--- src/CardTest.jsx
+++ src/CardTest.jsx
@@ -14,6 +14,7 @@ export default function CardTest() {
       setFlipped(false);
     }
   };
+
   const handleFlip = () => { flipped ? setFlipped(false) : setFlipped(true); };
 
   const handleKnownClick = () => {
@@ -26,6 +27,11 @@ export default function CardTest() {
     handleNextIndex();
   };
 
+  const handleRestart= () => {
+    setIndex(0);
+    setFlipped(false);
+  };
+
   
 
 
@@ -34,12 +40,13 @@ export default function CardTest() {
       <div className="h-full w-200 border-solid border-4 border-red-500">
         <div onClick={handleFlip} className="font-sans text-3xl flex justify-center items-center h-3/4 w-full border-dashed border-4 border-yellow-500">
           {flipped ? cards[index].meaning : cards[index].word}
-          <br />
-          {cards[index].learned ? "TrueY" : "Falsey"}
+          <br/>
+          {cards[index].learned ? "Learned" : "Not learned"}
         </div>
         <div className="font-sans text-3xl flex justify-around items-center h-1/4 w-full border-dashed border-4 border-green-500">
           <button onClick={handleUnknownClick}>I don't know</button>
           <button onClick={handleKnownClick}>I know</button>
+          <button onClick={handleRestart}>Restart</button>
         </div>
       </div>
     </div>
