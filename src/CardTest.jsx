8889e34c8d13b08fb9c7f39eea28f052efabe9ca
Noah Purdy <noahpurdy.dev@gmail.com>
2026-07-12 12:09:35 +0900

    Add logic for looping through cards and flipping them.

--- src/CardTest.jsx
+++ src/CardTest.jsx
@@ -2,21 +2,28 @@ import { useState } from "react";
 import flashcards from "./data/flashcards";
 
 export default function CardTest() {
-  const [card, setCard] = useState(flashcards[0].word);
-  const handleCardFlip = () => {
-    setCard(flashcards[0].meaning);
+  const [index, setIndex] = useState(0);
+  const [flipped, setFlipped] = useState(false);
+  const handleNextIndex = () => {
+    if (index < flashcards.length - 1) { 
+      setIndex(prev => prev + 1); 
+      setFlipped(false);
+    }
   };
+  const handleFlip = () => { flipped ? setFlipped(false) : setFlipped(true); };
+
+
+  
+
 
   return (
     <div className="h-screen border-solid border-4 border-blue-500 flex justify-center items-center">
       <div className="h-full w-200 border-solid border-4 border-red-500">
-        <div onClick={handleCardFlip} className="font-sans text-3xl flex justify-center items-center h-3/4 w-full border-dashed border-4 border-yellow-500">
-          {card}
+        <div onClick={handleFlip} className="font-sans text-3xl flex justify-center items-center h-3/4 w-full border-dashed border-4 border-yellow-500">
+          {flipped ? flashcards[index].meaning : flashcards[index].word}
         </div>
         <div className="font-sans text-3xl flex justify-around items-center h-1/4 w-full border-dashed border-4 border-green-500">
-          <button >Don't Know</button>
-          <button>Know</button>
-          <button>Skip</button>
+          <button onClick={handleNextIndex}>Next</button>
         </div>
       </div>
     </div>
