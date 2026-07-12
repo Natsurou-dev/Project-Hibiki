56489bc539d8311e5cfe528e7cb269364dc9dd2f
Noah Purdy <noahpurdy.dev@gmail.com>
2026-07-12 14:36:47 +0900

    Refactor code. Add logic for only displaying unlearned cards.

--- src/CardTest.jsx
+++ src/CardTest.jsx
@@ -1,52 +1,63 @@
 //you ought to know that, I think we're one and the same
 
-import { useState } from "react";
+import { use, useState } from "react";
 import flashcards from "./data/flashcards";
 
 export default function CardTest() {
   const [cards, setCards] = useState(flashcards);
   const [index, setIndex] = useState(0);
-  const [flipped, setFlipped] = useState(false);
+  const [flip, setFlip] = useState(false);
 
   const handleNextIndex = () => {
-    if (index < cards.length - 1) { 
-      setIndex(prev => prev + 1); 
-      setFlipped(false);
+    let nextIndex = index + 1;
+    if (nextIndex < cards.length) {
+      for (nextIndex; nextIndex < cards.length; nextIndex++) {
+        if (!cards[nextIndex].learned) { 
+          setIndex(nextIndex); 
+          break;
+        }
+      }
+      setFlip(false);
     }
-  };
+  }
 
-  const handleFlip = () => { flipped ? setFlipped(false) : setFlipped(true); };
+  const handleFlip = () => { flip ? setFlip(false) : setFlip(true); }
 
-  const handleKnownClick = () => {
-    if (!cards[index].learned) {cards[index].learned = true}
+  const handleRestart = () => { 
+    for (let i = 0; i < cards.length - 1; i++) {
+      if (!cards[i].learned) { 
+        setIndex(i); 
+        break;
+      }
+    }
+    setFlip(false);
+  }
+
+  const handleKnow = () => { 
+    cards[index].learned = true; 
     handleNextIndex();
-  };
+  }
 
-  const handleUnknownClick = () => {
-    if (cards[index].learned) {cards[index].learned = false}
+  const handleDontKnow = () => { 
+    cards[index].learned = false; 
     handleNextIndex();
-  };
-
-  const handleRestart= () => {
-    setIndex(0);
-    setFlipped(false);
-  };
-
-  
+  }
 
 
   return (
     <div className="h-screen border-solid border-4 border-blue-500 flex justify-center items-center">
       <div className="h-full w-200 border-solid border-4 border-red-500">
         <div onClick={handleFlip} className="font-sans text-3xl flex justify-center items-center h-3/4 w-full border-dashed border-4 border-yellow-500">
-          {flipped ? cards[index].meaning : cards[index].word}
-          <br/>
+          {flip ? cards[index].meaning : cards[index].word}
+          <br />
           {cards[index].learned ? "Learned" : "Not learned"}
+          <br />
+          {cards[index].id}
         </div>
         <div className="font-sans text-3xl flex justify-around items-center h-1/4 w-full border-dashed border-4 border-green-500">
-          <button onClick={handleUnknownClick}>I don't know</button>
-          <button onClick={handleKnownClick}>I know</button>
-          <button onClick={handleRestart}>Restart</button>
+          <button onClick={handleDontKnow}>I don't know</button>
+          <button onClick={handleKnow}>I know</button>
+          <button onClick={handleRestart}>Restart Cards</button>
         </div>
       </div>
     </div>
